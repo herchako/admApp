91de8fcc58618c2c657b084edf78d6e31e1fee48
herchako <hddk16@gmail.com>
2017-01-17 19:29:47 -0300

    propiedades e inquilinos view creadas

---
+++ AdmApp/AdmApp/Controllers/InquilinoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdmApp.DAL;
+using AdmApp.Models;
+
+namespace AdmApp.Controllers
+{
+    public class InquilinoController : Controller
+    {
+        private InmobiliariaContext db = new InmobiliariaContext();
+
+        // GET: Inquilino
+        public ActionResult Index()
+        {
+            return View(db.Inquilinos.ToList());
+        }
+
+        // GET: Inquilino/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inquilino inquilino = db.Inquilinos.Find(id);
+            if (inquilino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inquilino);
+        }
+
+        // GET: Inquilino/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inquilino/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre,Apellido,FechaDeAlta")] Inquilino inquilino)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inquilinos.Add(inquilino);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inquilino);
+        }
+
+        // GET: Inquilino/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inquilino inquilino = db.Inquilinos.Find(id);
+            if (inquilino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inquilino);
+        }
+
+        // POST: Inquilino/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,Apellido,FechaDeAlta")] Inquilino inquilino)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inquilino).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inquilino);
+        }
+
+        // GET: Inquilino/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inquilino inquilino = db.Inquilinos.Find(id);
+            if (inquilino == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inquilino);
+        }
+
+        // POST: Inquilino/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inquilino inquilino = db.Inquilinos.Find(id);
+            db.Inquilinos.Remove(inquilino);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AdmApp/AdmApp/Controllers/PropiedadController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdmApp.DAL;
+using AdmApp.Models;
+
+namespace AdmApp.Controllers
+{
+    public class PropiedadController : Controller
+    {
+        private InmobiliariaContext db = new InmobiliariaContext();
+
+        // GET: Propiedad
+        public ActionResult Index()
+        {
+            var propiedades = db.Propiedades.Include(p => p.Inquilino).Include(p => p.Locador);
+            return View(propiedades.ToList());
+        }
+
+        // GET: Propiedad/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Propiedad propiedad = db.Propiedades.Find(id);
+            if (propiedad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(propiedad);
+        }
+
+        // GET: Propiedad/Create
+        public ActionResult Create()
+        {
+            ViewBag.InquilinoID = new SelectList(db.Inquilinos, "ID", "Nombre");
+            ViewBag.LocadorID = new SelectList(db.Locadores, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: Propiedad/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,LocadorID,InquilinoID,Calle,Altura")] Propiedad propiedad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Propiedades.Add(propiedad);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InquilinoID = new SelectList(db.Inquilinos, "ID", "Nombre", propiedad.InquilinoID);
+            ViewBag.LocadorID = new SelectList(db.Locadores, "ID", "Nombre", propiedad.LocadorID);
+            return View(propiedad);
+        }
+
+        // GET: Propiedad/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Propiedad propiedad = db.Propiedades.Find(id);
+            if (propiedad == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InquilinoID = new SelectList(db.Inquilinos, "ID", "Nombre", propiedad.InquilinoID);
+            ViewBag.LocadorID = new SelectList(db.Locadores, "ID", "Nombre", propiedad.LocadorID);
+            return View(propiedad);
+        }
+
+        // POST: Propiedad/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,LocadorID,InquilinoID,Calle,Altura")] Propiedad propiedad)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(propiedad).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InquilinoID = new SelectList(db.Inquilinos, "ID", "Nombre", propiedad.InquilinoID);
+            ViewBag.LocadorID = new SelectList(db.Locadores, "ID", "Nombre", propiedad.LocadorID);
+            return View(propiedad);
+        }
+
+        // GET: Propiedad/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Propiedad propiedad = db.Propiedades.Find(id);
+            if (propiedad == null)
+            {
+                return HttpNotFound();
+            }
+            return View(propiedad);
+        }
+
+        // POST: Propiedad/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Propiedad propiedad = db.Propiedades.Find(id);
+            db.Propiedades.Remove(propiedad);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
