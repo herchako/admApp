26cec131984a4b7981d371c001376223b62632da
herchako <hddk16@gmail.com>
2017-02-06 17:23:39 -0300

    ultimo commit pre agregado de contratos y deudas

--- AdmApp/AdmApp/Models/Inquilino.cs
+++ AdmApp/AdmApp/Models/Inquilino.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AdmApp.Models
@@ -11,12 +12,15 @@ namespace AdmApp.Models
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Telefono Particular")]
         public string Telefono { get; set; }
         public string Celular { get; set; }
+        [Display(Name = "Domicilio Particular")]
         public string Direccion { get; set; }
         public string Observaciones { get; set; }
+        [Display(Name = "Fecha de Alta")]
         public DateTime FechaDeAlta { get; set; }
-        
+
         public virtual ICollection<Propiedad> Propiedades { get; set; }
     }
 }
